4e8945f9cd35612a0085425f487a67b177888f77
zhouyc <602324071@qq.com>
2017-05-23 11:03:50 +0800

    2.提交初始化项目

---
+++ src/test/java/TestGradle.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by lx on 2017/5/23.
+ */
+public class TestGradle {
+
+    @Test
+    public void testaa(){
+        System.out.println("12345");
+    }
+}
